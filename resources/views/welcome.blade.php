6e92abb2b247243df9505b3e062a5f9d6ce8fe5c
nitolpalak <nitolpalak@gmail.com>
2017-12-21 04:24:36 +0600

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Restaurant Feedback</title>
-
+        <!-- na na na nare nare -->
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
